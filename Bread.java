8b7308f32a3512198101f40cd26fd63ce70bafa0
Kiệt Tuấn <tuankietng61@gmail.com>
2025-04-29 10:04:57 +0700

    Add files via upload

---
+++ Bread.java
@@ -0,0 +1,11 @@
+package hus.oop.decorator;
+
+public abstract class Bread {
+	protected String description;
+  
+	public String getDescription() {
+		/* TODO */
+	}
+ 
+	public abstract double cost();
+}
---
+++ ThickcrustBread.java
@@ -0,0 +1,13 @@
+package hus.oop.decorator;
+
+public class ThickcrustBread extends Bread {
+  
+	public ThickcrustBread() {
+		description = "ThickcrustBread";
+	}
+  
+	public double cost() {
+		return 30.0;
+	}
+}
+
